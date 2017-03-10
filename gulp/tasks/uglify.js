df8d3948051194a697ef9f599b8deeef57a9d69d
mayiwen <472953586@qq.com>
2017-03-10 10:20:55 +0800

    ；

--- gulp/tasks/uglify.js
+++ gulp/tasks/uglify.js
@@ -7,7 +7,7 @@ gulp.task('uglify', function(){
 	return gulp.src(config.src)
 		.pipe(uglify())
 		.pipe(rev())
-		.pipe(gulp.dest(config.dest));
+		.pipe(gulp.dest(config.dest))
 		.pipe(rev.manifest()) //set hash key json
 		.pipe(gulp.dest(config.rev)); //dest hash key json
 });
